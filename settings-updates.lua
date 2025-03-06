9f7ed6289dd0f112476830ba7245ea3f6076805b
sapphiriza <sapphiriza@gmail.com>
2025-03-05 23:46:58 -0800

    Compatibility with Wooden Universe

--- settings-updates.lua
+++ settings-updates.lua
@@ -1,1 +1,9 @@
 data.raw["bool-setting"]["PlanetsLib-enable-temperature"].forced_value = true
+
+if mods["wood-logistics"] then
+  if data.raw["int-setting"]["wood-logistics-rail-cost"] then
+    data.raw["int-setting"]["wood-logistics-rail-cost"].hidden = true
+    data.raw["int-setting"]["wood-logistics-rail-cost"].default_value = 0
+    data.raw["int-setting"]["wood-logistics-rail-cost"].allowed_values = {0}
+  end
+end
