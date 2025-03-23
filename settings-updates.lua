7e1616df5267755792c7ee52f8c18ae957d73d55
sh4dow <shadoxxhd@users.noreply.github.com>
2025-03-23 17:30:28 +0100

    Update settings-updates.lua
    move setting definition to settings.lua

--- settings-updates.lua
+++ settings-updates.lua
@@ -7,13 +7,3 @@ if mods["wood-logistics"] then
     data.raw["int-setting"]["wood-logistics-rail-cost"].allowed_values = {0}
   end
 end
-if mods["maraxsis"] then
-  data:extend({
-    {
-        type = "bool-setting",
-        name = "aop-merge-hydro",
-        setting_type = "startup",
-        default_value = true
-    }
-  })
-end
