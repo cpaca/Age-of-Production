7fc5c2863de176b0ac4bb3c255ae73a0691b61b0
Andreus <andreusaxolotl@gmail.com>
2025-04-20 09:47:49 +0200

    wefwfw

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,3 +2,13 @@ require "compat.foundry-restructuring"
 
 local meld = require("meld")
             data.raw["lab"]["aop-quantum-computer"].inputs = meld(data.raw["lab"]["aop-quantum-computer"].inputs, data.raw["lab"]["lab"].inputs)
+
+data.raw.recipe["nutrients-recycling"].results = {
+                {
+                  type = "item",
+                  name = "spoilage",
+                  amount = 2.5,
+                  extra_count_fraction = 0.5
+                }
+          }
+          data.raw.recipe["nutrients-recycling"].energy_required = 0.125
