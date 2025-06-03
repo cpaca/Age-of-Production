3e33cbd9ed2db40ff7127d8c760058a0e7c1ab6e
Andreus <andreusaxolotl@gmail.com>
2025-06-03 17:10:45 +0200

    coralmium

--- data-updates.lua
+++ data-updates.lua
@@ -19,3 +19,13 @@ if mods["bobassembly"] then
       add_crafting_categories("assembling-machine", "bob-steam-assembling-machine", {"advanced-centrifuging-or-crafting", "advanced-centrifuging-or-crafting-or-electromechanics", "quantum-assembling-or-crafting", "woodworking-or-crafting", "electromechanics-or-crafting", "biochemistry-or-organic-or-assembling", "woodworking-or-organic-or-assembling", "petrochemistry-or-organic-or-assembling" , "electronics-or-electromechanics", "pressing-or-electromechanics", "quantum-assembling-or-crafting-with-fluid", "advanced-centrifuging-or-crafting-with-fluid", "ammunition-or-crafting", "synthesis-or-crafting-with-fluid", "organic-or-assembling-or-electromechanics", "ammunition-or-crafting-with-fluid"})
   end
 end
+
+ if mods["fulgora-coralmium-agriculture"] then
+ if not mods["wood-industry"] then
+  add_crafting_categories("assembling-machine", "chemical-plant", {"woodworking-or-organic"})
+ end
+ if mods["wood-industry"] then
+  data.raw.recipe["burnt-spoilage"].category = "organic-or-kiln-smelting"
+  add_crafting_categories("assembling-machine", "aop-lumber-mill", {"organic-or-kiln-smelting"})
+ end
+end
