b49ad179b8cbe39e679de1860b49b6430814df7c
Andreus <andreusaxolotl@gmail.com>
2025-06-19 16:43:02 +0200

    brrr

--- prototypes/miscellaneous-recipes.lua
+++ prototypes/miscellaneous-recipes.lua
@@ -25,13 +25,13 @@ data:extend {{
     icon = "__Age-of-Production-Graphics__/graphics/icons/uranium-sifting.png",
     subgroup = "uranium-processing",
     enabled = false,
-    energy_required = 20,
+    energy_required = 24,
     ingredients = {
         {type = "item", name = "stone",      amount = 10},
     },
     results = {
-        {type = "item", name = "uranium-238", amount = 5, probability = 0.1},
-        {type = "item", name = "uranium-235", amount = 1, probability = 0.01},
+        {type = "item", name = "uranium-238", amount = 1, probability = 0.049},
+        {type = "item", name = "uranium-235", amount = 1, probability = 0.0001},
         },
     allow_productivity = true,
     category = "advanced-centrifuging",
